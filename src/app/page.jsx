eda102cc751adcc0c4993abe44f9500627ef6372
Vaibhav Bhardwaj <vaibhavbhardwaj1230@gmail.com>
2023-07-21 20:40:55 +0530

    refactor

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,13 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className={styles.texts}>
+        <h1></h1>
+        <p></p>
+        <button></button>
+      </div>
+      <div className={styles.image}></div>
+    </div>
   )
 }
