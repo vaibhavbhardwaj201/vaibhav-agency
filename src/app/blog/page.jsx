06f0d6f0f55567bb4080492b4b043c4a4e2d1f7e
Vaibhav Bhardwaj <vaibhavbhardwaj1230@gmail.com>
2023-07-25 09:09:41 +0530

    ADD: Blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
 import styles from './blog.module.css'
 
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
+    <div className={styles.container}>
+      <Link href='/blog/test' className={styles.itemContainer}>
+        <div className={styles.imageContainer}>
+          <Image
+            src='https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg'
+            alt='Blog Image'
+            width={400}
+            height={250}
+            className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.heading}>This is heading</h1>
+          <p className={styles.description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, cupiditate delectus labore vitae ullam corporis tenetur cumque culpa soluta voluptatum officiis incidunt a ipsam, dignissimos, repudiandae ipsa facere? Nobis itaque eos quo delectus atque commodi autem blanditiis ex, provident voluptates ullam quidem magnam a, reiciendis vel doloribus maiores iste harum! Voluptatum quo ullam cum ea non repudiandae quasi? Accusantium ipsa doloribus ipsam nemo nam sint, cumque velit veniam vel eum fugiat consectetur expedita earum unde, architecto rem, eveniet deserunt ullam perspiciatis itaque hic mollitia magni culpa molestiae. Culpa, dicta eaque!</p>
+        </div>
+      </Link>
+      <Link href='/blog/test' className={styles.itemContainer}>
+        <div className={styles.imageContainer}>
+          <Image
+            src='https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg'
+            alt='Blog Image'
+            width={400}
+            height={250}
+            className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.heading}>This is heading</h1>
+          <p className={styles.description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, cupiditate delectus labore vitae ullam corporis tenetur cumque culpa soluta voluptatum officiis incidunt a ipsam, dignissimos, repudiandae ipsa facere? Nobis itaque eos quo delectus atque commodi autem blanditiis ex, provident voluptates ullam quidem magnam a, reiciendis vel doloribus maiores iste harum! Voluptatum quo ullam cum ea non repudiandae quasi? Accusantium ipsa doloribus ipsam nemo nam sint, cumque velit veniam vel eum fugiat consectetur expedita earum unde, architecto rem, eveniet deserunt ullam perspiciatis itaque hic mollitia magni culpa molestiae. Culpa, dicta eaque!</p>
+        </div>
+      </Link>
+      <Link href='/blog/test' className={styles.itemContainer}>
+        <div className={styles.imageContainer}>
+          <Image
+            src='https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg'
+            alt='Blog Image'
+            width={400}
+            height={250}
+            className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.heading}>This is heading</h1>
+          <p className={styles.description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, cupiditate delectus labore vitae ullam corporis tenetur cumque culpa soluta voluptatum officiis incidunt a ipsam, dignissimos, repudiandae ipsa facere? Nobis itaque eos quo delectus atque commodi autem blanditiis ex, provident voluptates ullam quidem magnam a, reiciendis vel doloribus maiores iste harum! Voluptatum quo ullam cum ea non repudiandae quasi? Accusantium ipsa doloribus ipsam nemo nam sint, cumque velit veniam vel eum fugiat consectetur expedita earum unde, architecto rem, eveniet deserunt ullam perspiciatis itaque hic mollitia magni culpa molestiae. Culpa, dicta eaque!</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -14,7 +14,21 @@ const Category = ({params}) => {
       <div className={styles.items}>
         <div className={styles.textContainer}>
           <h1 className={styles.heading}>Heading</h1>
-          <p className={styles.description}>This is paragraph</p>
+          <p className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate iusto, fugit id perspiciatis distinctio officiis nisi quae, vitae repellendus, libero cupiditate laborum qui minima numquam molestias eius consequuntur. Vero quisquam quam error omnis deserunt aut ratione sed possimus hic expedita.</p>
+          <Button url='#' text='See More' />
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            className={styles.image}
+            src='https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg'
+            alt='Image'
+            fill={true} />
+        </div>
+      </div>
+      <div className={styles.items}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.heading}>Heading</h1>
+          <p className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate iusto, fugit id perspiciatis distinctio officiis nisi quae, vitae repellendus, libero cupiditate laborum qui minima numquam molestias eius consequuntur. Vero quisquam quam error omnis deserunt aut ratione sed possimus hic expedita.</p>
           <Button url='#' text='See More' />
         </div>
         <div className={styles.imageContainer}>
