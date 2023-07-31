eea7f7a551fdea750c92ba208eac11e9b81887ad
Vaibhav Bhardwaj <vaibhavbhardwaj1230@gmail.com>
2023-07-31 23:02:03 +0530

    feat: NextAuth handler and Dashboard page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react'
 import styles from './dashboard.module.css'
 
 import useSWR from 'swr'
+import { useSession } from 'next-auth/react'
 
 const Dashboard = () => {
 
@@ -31,6 +32,10 @@ const Dashboard = () => {
   //   getData()
   // }, [])
   
+
+  const session = useSession()
+
+  console.log(session);
 
   const fetcher = (...args) => fetch(...args).then(res => res.json())
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,33 @@
-import Navbar from '@/components/navbar/Navbar'
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Footer from '@/components/footer/Footer'
-import { ThemeProvider } from '@/context/ThemeContext'
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
+import { ThemeProvider } from "@/context/ThemeContext";
 
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from "next/font/google";
+import { SessionProvider } from "next-auth/react";
+import AuthProvider from "@/components/authprovider/AuthProvider";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'VebShen',
-  description: 'Blogging site',
-}
+  title: "VebShen",
+  description: "Blogging site",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider>
-          <div className='container'>
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
+          <AuthProvider>
+            <div className="container">
+              <Navbar />
+              {children}
+              <Footer />
+            </div>
+          </AuthProvider>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
