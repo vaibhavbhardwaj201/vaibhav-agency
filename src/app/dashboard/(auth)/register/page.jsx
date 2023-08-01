3f4cb56edb7d66396641d86feb3a25aa190f94d1
Vaibhav Bhardwaj <vaibhavbhardwaj1230@gmail.com>
2023-08-01 23:59:48 +0530

    register page

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+
+import styles from "./register.module.css";
+import Link from "next/link";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          className={styles.input}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          className={styles.input}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          className={styles.input}
+          required
+        />
+        <button className={styles.button}>Register</button>
+      </form>
+      <p className={styles.loginText}>Already have an Account? <Link href='/dashboard/login'><span className={styles.text}>Login</span></Link></p>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
--- src/components/ui/button/Button.jsx
+++ src/components/ui/button/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
+import Link from 'next/link'
 
 import styles from './button.module.css'
-import Link from 'next/link'
 
 const Button = ({text, url}) => {
   return (
