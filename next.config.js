152aef40834f295f639fed9e03dab145fca14014
Vaibhav Bhardwaj <vaibhavbhardwaj1230@gmail.com>
2023-10-10 05:25:23 +0300

    minor

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
     images: {
         domains: ["images.pexels.com", "images.unsplash.com"],
     },
+    webpack: (config) => {
+    config.resolve.fallback = {
+      "mongodb-client-encryption": false ,
+      "aws4": false
+    };
+
+    return config;
+  }
+
 }
 
 module.exports = nextConfig
